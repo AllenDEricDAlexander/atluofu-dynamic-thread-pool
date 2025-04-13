9a3b855ec630ff6f5ba77f49b131f9f6ecffd7bf
luofu <mqa010225@163.com>
2025-04-13 17:36:47 +0800

    <feat>(<dynamic-thread-pool>):<starter>
    - test

--- atluofu-dynamic-thread-pool-test/src/test/java/ApiTest.java
+++ atluofu-dynamic-thread-pool-test/src/test/java/ApiTest.java
@@ -1,4 +1,14 @@
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.redisson.api.RTopic;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import top.atluofu.middleware.dynamic.thread.pool.sdk.domain.model.entity.ThreadPoolConfigEntity;
 
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @ClassName: ApiTest
@@ -7,5 +17,22 @@
  * @datetime: 2025Year-01Month-05Day-21:15
  * @Version: 1.0
  */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ApiTest {
+
+    @Resource
+    private RTopic dynamicThreadPoolRedisTopic;
+
+    @Test
+    public void test_dynamicThreadPoolRedisTopic() throws InterruptedException {
+        ThreadPoolConfigEntity threadPoolConfigEntity = new ThreadPoolConfigEntity("dynamic-thread-pool-test-app", "threadPoolExecutor01");
+        threadPoolConfigEntity.setPoolSize(100);
+        threadPoolConfigEntity.setMaximumPoolSize(100);
+        dynamicThreadPoolRedisTopic.publish(threadPoolConfigEntity);
+        new CountDownLatch(1).await();
+    }
+
+
 }
